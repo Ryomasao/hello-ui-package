0c781433fd4b6f34f33b1d16c39fafda8eae0777
Ryomasao <konoemario@gmail.com>
2021-07-13 19:55:58 +0900

    playground

--- playground/src/main.tsx
+++ playground/src/main.tsx
@@ -1,8 +1,10 @@
 import * as ReactDOM from 'react-dom'
+import { Button } from '@ryomasao/hello-ui-package'
 
 const App = () => {
   return (
     <div>
+      <Button color={'yellow'}></Button>
       <p>hello react</p>
     </div>
   )
--- playground/webpack.config.js
+++ playground/webpack.config.js
@@ -17,6 +17,9 @@ module.exports = {
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    alias: {
+      react: path.resolve('./node_modules/react'),
+    },
   },
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
